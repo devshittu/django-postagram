413e7b46cf42dd28b50b86b66d298037e9135f52
Muhammed Shittu <devshittu@gmail.com>
2023-03-14 10:11:45 +0000

    user manager class implemented.

--- core/user/models.py
+++ core/user/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 import uuid
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import Http404
 
 class User(AbstractBaseUser, PermissionsMixin):
     public_id = models.UUIDField(
@@ -25,3 +27,44 @@ class User(AbstractBaseUser, PermissionsMixin):
     @property
     def name(self):
         return f"{self.first_name} {self.last_name}"
+
+
+class UserManager(BaseUserManager):
+    def get_object_by_public_id(self, public_id):
+        try:
+            instance = self.get(public_id=public_id)
+            return instance
+        except (ObjectDoesNotExist, ValueError, TypeError):
+            return Http404
+
+    def create_user(self, username, email, password=None, **kwargs):
+        """Create and return a `User` with an email, phone 
+        number, username and password."""
+        if username is None:
+            raise TypeError('Users must have a username.')
+        if email is None:
+            raise TypeError('Users must have an email.')
+        if password is None:
+            raise TypeError('User must have an email.')
+        user = self.model(username=username, email=self.normalize_email(email), **kwargs)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password, **kwargs):
+        """
+        Create and return a `User` with superuser (admin)
+        permissions.
+        """
+        if password is None:
+            raise TypeError('Superusers must have a password.')
+        if email is None:
+            raise TypeError('Superusers must have an email.')
+        if username is None:
+            raise TypeError('Superusers must have an username.')
+
+        user = self.create_user(username, email, password, **kwargs)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
