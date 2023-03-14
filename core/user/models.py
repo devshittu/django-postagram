030727f881615c710a4e1a080500fc9e3d57e603
Muhammed Shittu <devshittu@gmail.com>
2023-03-14 09:48:30 +0000

    user models implemented.

--- core/user/models.py
+++ core/user/models.py
@@ -1,1 +1,27 @@
 from django.db import models
+import uuid
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
+class User(AbstractBaseUser, PermissionsMixin):
+    public_id = models.UUIDField(
+        db_index=True, unique=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(db_index=True, max_length=255, unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(db_index=True, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    objects = UserManager()
+
+    def __str__(self) -> str:
+        return f"{self.email}"
+
+    @property
+    def name(self):
+        return f"{self.first_name} {self.last_name}"
